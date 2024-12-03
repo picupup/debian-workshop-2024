9adbf52221bc2661d584dadb47b2050d772a2cd3
picupup <44735171+picupup@users.noreply.github.com>
2024-12-03 11:51:57 +0100

    check for password in init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,15 @@
 # set -x # Uncomment to debug
 # set -n # Uncomment to check script syntax without execution
 
+pass=secrets/root_password
+mkdir -p secrets
+touch ${pass}
+
+if [ ! -f ${pass} -o -z "$(cat ${pass})" ]; then
+        echo "Error: Bitte stelle sicher dass '${pass}' existert und den root passwort enthält" >&2
+        exit 1
+fi
+
 ./open-ports.sh
 
 ./build.sh -r
